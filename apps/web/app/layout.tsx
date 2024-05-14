02aff1a852c411e82731a870e916aedcbbe87142
mmopinodo <mm@opinodo.com>
2024-05-14 12:28:54 +0200

    fix: google tag manager

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,3 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Metadata } from "next";
 
@@ -15,9 +14,28 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PJ6M9K9P');`,
+          }}
+        />
+      </head>
       {process.env.VERCEL === "1" && <SpeedInsights sampleRate={0.1} />}
-      <body className="flex h-screen flex-col">{children}</body>
-      <GoogleAnalytics gaId="G-R7W0H3MQ42" />
+      <body className="flex h-screen flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PJ6M9K9P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
