d0502517bd7bf9e7c16e5c36e5568e60ed78a31c
mmopinodo <mm@opinodo.com>
2024-06-03 13:59:48 +0200

    chore: removed formbricks branding from survey inactive page

--- apps/web/app/s/[surveyId]/components/SurveyInactive.tsx
+++ apps/web/app/s/[surveyId]/components/SurveyInactive.tsx
@@ -1,11 +1,8 @@
 import { CheckCircle2Icon, HelpCircleIcon, PauseCircleIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 import { TSurveyClosedMessage } from "@formbricks/types/surveys";
 import { Button } from "@formbricks/ui/Button";
-
-import footerLogo from "../lib/footerlogo.svg";
 
 export const SurveyInactive = ({
   status,
@@ -41,14 +38,12 @@ export const SurveyInactive = ({
         </p>
         {!(status === "completed" && surveyClosedMessage) && status !== "link invalid" && (
           <Button variant="darkCTA" className="mt-2" href="https://formbricks.com">
-            Create your own
+            Join DigiOpinion
           </Button>
         )}
       </div>
       <div>
-        <Link href="https://formbricks.com">
-          <Image src={footerLogo} alt="Brand logo" className="mx-auto w-40" />
-        </Link>
+        <Link href="https://digiopinion.com"></Link>
       </div>
     </div>
   );
