1dabf2713f40c2957e38102e91c1d0211ac84453
mmopinodo <mm@opinodo.com>
2025-02-26 13:15:11 +0100

    branding on not found page fixed

--- apps/web/modules/survey/link/not-found.tsx
+++ apps/web/modules/survey/link/not-found.tsx
@@ -1,9 +1,8 @@
+import OpinodoLogo from "@/images/opinodo-logo.png";
 import { Button } from "@/modules/ui/components/button";
 import { HelpCircleIcon } from "lucide-react";
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import Link from "next/link";
-import footerLogo from "./lib/footerlogo.svg";
 
 export const LinkSurveyNotFound = () => {
   return (
@@ -14,12 +13,12 @@ export const LinkSurveyNotFound = () => {
         <h1 className="text-4xl font-bold text-slate-800">Survey not found.</h1>
         <p className="text-lg leading-10 text-slate-500">There is no survey with this ID.</p>
         <Button className="mt-2" asChild>
-          <Link href="https://formbricks.com">Create your own</Link>
+          <Link href="https://member.digiopinion.com/overview">Take more surveys</Link>
         </Button>
       </div>
       <div>
-        <Link href="https://formbricks.com">
-          <Image src={footerLogo as StaticImport} alt="Brand logo" className="mx-auto w-40" />
+        <Link href="https://digiopinion.com">
+          <Image src={OpinodoLogo} alt="Brand logo" className="mx-auto w-40" />
         </Link>
       </div>
     </div>
