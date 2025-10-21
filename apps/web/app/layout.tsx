3923dcc4c8602cff828d80c262ea824abf24e542
mmopinodo <mm@opinodo.com>
2025-10-21 13:26:37 +0200

    fix3: changed google tag manager implementation

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,7 +5,6 @@ import { getLocale } from "@/tolgee/language";
 import { getTolgee } from "@/tolgee/server";
 import { TolgeeStaticData } from "@tolgee/react";
 import { Metadata } from "next";
-import Script from "next/script";
 import React from "react";
 import "../modules/ui/globals.css";
 
@@ -25,23 +24,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html lang={locale} translate="no">
-      <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PJ6M9K9P');`}
-        </Script>
-      </head>
       <body className="flex h-dvh flex-col transition-all ease-in-out">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PJ6M9K9P"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}></iframe>
-        </noscript>
         <SentryProvider
           sentryDsn={SENTRY_DSN}
           sentryRelease={SENTRY_RELEASE}
